4facb2439afd6163dd930ca87d0f53996fa76005
Taro Hayashi <87086895+Taro-Hayashi@users.noreply.github.com>
2022-06-22 10:46:29 +0900

    restore

--- keyboards/tarohayashi/popntop_np/rules.mk
+++ keyboards/tarohayashi/popntop_np/rules.mk
@@ -10,7 +10,7 @@ SPLIT_KEYBOARD = yes
 
 # ETC
 BOOTMAGIC_ENABLE = no     # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes       # Mouse keys
+MOUSEKEY_ENABLE = no       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
