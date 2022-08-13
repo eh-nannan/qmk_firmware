16fcf5e2bd504622f830b3ca3399cdfcf294ccb7
Taro Hayashi <87086895+Taro-Hayashi@users.noreply.github.com>
2022-08-13 11:00:30 +0900

    remove led timelimit

--- keyboards/tarohayashi/handyman/keymaps/mylayout/keymap.c
+++ keyboards/tarohayashi/handyman/keymaps/mylayout/keymap.c
@@ -12,14 +12,14 @@ enum custom_keycode {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [CLIPSTUDIO] = LAYOUT(
           G(KC_S), G(KC_C), G(KC_V), G(KC_X), KC_BSPC,
-        LSFT(KC_F7), LCTL(KC_M), LCTL(KC_T),  LCTL(KC_D), KC_M,
-        KC_X, KC_F13, LCTL(KC_Z), KC_F14, LCTL(KC_0),
-        KC_O, KC_S, KC_Y, KC_I, KC_F15,
-        LCTL(KC_H), LT(LIGHT, KC_U), LALT_T(KC_E), LCTL_T(KC_B), LSFT_T(KC_P),
+        LSG(KC_I), G(KC_U), G(KC_T),  G(KC_D), G(KC_C),
+        KC_X, G(KC_0), G(KC_Z), KC_9, G(KC_V),
+        KC_O, KC_S, KC_Y, KC_I, LAG(KC_0),
+        KC_H, LT(LIGHT, KC_U), LALT_T(KC_E), GUI_T(KC_B), LSFT_T(KC_P),
         // LEVER
         XXXXXXX, KC_T, KC_G, KC_J, XXXXXXX,
         // ENCODER
-        LCTL(KC_KP_PLUS), LCTL(KC_KP_MINUS),   KC_LBRC,KC_RBRC,  KC_MINS, KC_QUOT
+        LGUI(KC_KP_PLUS), LGUI(KC_KP_MINUS),   KC_LBRC,KC_RBRC,  KC_MINS, KC_QUOT
   ),
   [PHOTOSHOP] = LAYOUT(
         G(KC_S), G(KC_C), G(KC_V), G(KC_X), KC_BSPC,
