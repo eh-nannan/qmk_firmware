474f929f205e0cf3de6fe2316847f68a8cabed94
Taro Hayashi <87086895+Taro-Hayashi@users.noreply.github.com>
2024-12-11 00:53:46 +0900

    Update keymap.c

--- keyboards/tarohayashi/shotgun_v2/keymaps/default/keymap.c
+++ keyboards/tarohayashi/shotgun_v2/keymaps/default/keymap.c
@@ -2,13 +2,14 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
+#include "lib/add_keycodes.h"
 
 // キーマップ
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-      KC_A, KC_B, KC_C, KC_D, KC_E, KC_F, 
-      KC_G, KC_H, KC_I, KC_J, KC_K, KC_L, 
-      KC_M, KC_N, KC_O, KC_P, KC_Q, KC_R
+      KC_PSLS, KC_P7, KC_P8, KC_P9, KC_BSPC,
+      KC_PAST, KC_P4, KC_P5, KC_P6, KC_TAB,
+      KC_PMNS, KC_P1, KC_P2, KC_P3, KC_PENT,
+      KC_PPLS,     KC_P0,    KC_PDOT
     )
 };
-
